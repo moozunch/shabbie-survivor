4ee8b83c40edb9429fc7999f40cc071c7d0db846
Sabitwise <atikahss2006@gmail.com>
2025-12-06 12:20:38 +0700

    create garlic and knife weapons

---
+++ Assets/Scripts/Weapons/Weapon Base/WeaponController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// base script for all weapons
+
+public class WeaponController : MonoBehaviour
+{
+    [Header("Weapon Stats")]
+    public WeaponScriptableObject weaponData;
+    float currentCooldown;
+
+    protected PlayerMovement pm;
+
+    protected virtual void Start()
+    {
+        pm = FindObjectOfType<PlayerMovement>();
+        currentCooldown = weaponData.CooldownDuration;
+    }
+    
+    protected virtual void Update()
+    {
+        currentCooldown -= Time.deltaTime;
+        if(currentCooldown <= 0f){
+            Attack();
+        }
+    }
+
+    protected virtual void Attack(){
+        currentCooldown = weaponData.CooldownDuration;
+    }
+}
